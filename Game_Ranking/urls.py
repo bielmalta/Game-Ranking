15dc047bad8378238a3196d108ffd0fa21f65b07
Arthur Reis <afsr@cesar.school>
2026-05-12 20:56:10 -0300

    feat: add Google Search Console verification URL

--- Game_Ranking/urls.py
+++ Game_Ranking/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('games.urls')),
-    path('reviews/', include('reviews.urls')),
-    path('accounts/', include('accounts.urls')),
+        path('admin/', admin.site.urls),
+        path('google91eb3706ff299c8c.html', TemplateView.as_view(template_name='google91eb3706ff299c8c.html', content_type='text/html')),
+        path('', include('games.urls')),
+        path('reviews/', include('reviews.urls')),
+        path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
