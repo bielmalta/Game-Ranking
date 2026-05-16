3268a7772659b9b9351d911ffd5f5a3980bd4407
Arthur Reis <afsr@cesar.school>
2026-05-15 22:19:35 -0300

    Adiciona logo e favicon do Game Ranking

--- Game_Ranking/urls.py
+++ Game_Ranking/urls.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
         path('admin/', admin.site.urls),
+        path(
+                'favicon.ico',
+                RedirectView.as_view(
+                        url=f'{settings.STATIC_URL}games/img/game-ranking-icon.png',
+                        permanent=True,
+                ),
+                name='favicon',
+        ),
         path('google91eb3706ff299c8c.html', TemplateView.as_view(template_name='google91eb3706ff299c8c.html', content_type='text/html')),
         path('', include('games.urls')),
         path('reviews/', include('reviews.urls')),
